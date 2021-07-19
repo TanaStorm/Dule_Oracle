af9705c0c412bbc1745c7823bde5dc5344ab9ed2
Skex420 <esteban.salasc@outlook.com>
2021-07-18 19:50:15 -0600

    modified OCI parameters

--- php/loginverificacion.php
+++ php/loginverificacion.php
@@ -12,24 +12,21 @@
 	session_start();
     // If form submitted, insert values into the database.
     if (isset($_POST['username'])){
-		
-		$username = stripslashes($_REQUEST['username']); // removes backslashes
-		$username = mysqli_real_escape_string($con,$username); //escapes special characters in a string
-		$password = stripslashes($_REQUEST['password']);
-		$password = mysqli_real_escape_string($con,$password);
-		debug_to_console($username);
-		debug_to_console($password);
-		
-	//Checking is user existing in the database or not
-        $query = "SELECT * FROM `usuario` WHERE email='$username' and contraseña='".md5($password)."'";
-		$result = mysqli_query($con,$query) or die(mysql_error());
-		$rows = mysqli_num_rows($result);
-        if($rows==1){
-			$_SESSION['username'] = $username;
-			header("Location: shop.php"); // Redirect user to index.php
-            }else{
+			$username = stripslashes($_REQUEST['username']); // removes backslashes
+			$password = stripslashes($_REQUEST['password']);
+			debug_to_console($username);
+			debug_to_console($password);
+			
+		//Checking is user existing in the database or not
+			$sql = "SELECT * FROM `usuario` WHERE email='$username' and contraseña='".md5($password)."'";
+			$parse = oci_parse($con,$sql) or die(oci_error());
+			$rows = oci_num_rows($parse);
+			if($rows==1){
+				$_SESSION['username'] = $username;
+				header("Location: shop.php"); // Redirect user to index.php
+			}else{
 				echo "<br/><div class='form'><h3>El usuario o la contraseña son incorrectos</h3>
 				<br/>Haga clic para intentarlo de nuevo to <a href='login.php'>Inicio de sesión</a></div>";
-				}
-    }else{
+			}
+		}
 ?>
